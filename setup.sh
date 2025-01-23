c2521857db130cc36499512871627e588c41cf70
Kelwinkxps13 <kelwinjhackson13@gmail.com>
2025-01-23 10:37:44 -0300

    adicionado o setup.sh

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Executando composer self-update"
+composer self-update
+
+echo "Executando composer update"
+composer update
+
+echo "Executando composer install"
+composer install
+
+echo "Executando npm install"
+npm install
+
+echo "Executando npm run build"
+npm run build
+
+echo "Copiando .env.example para .env"
+cp .env.example .env
+
+echo "Gerando API Key"
+php artisan key:generate
+
+echo "Criando tabelas no banco de dados local"
+php artisan migrate
+
+echo "Processo concluído."
+read -p "Pressione [Enter] para continuar..."
