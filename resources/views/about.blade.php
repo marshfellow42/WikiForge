3921aed1bff68c058e5bc9dd3ada5e5de4434cd1
marshfellow42 <123888466+marshfellow42@users.noreply.github.com>
2025-02-17 20:39:24 -0300

    Atualização das funções de cada membro da equipe

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,13 +9,13 @@
 <div class="col-md-4">
     <div class="text-center">
         <h1>Kelwin</h1>
-        <h3>Co-Fundador e Principal Programador Back-End</h3>
+        <h3>Co-Fundador e Programador Back-End</h3>
     </div>
 </div>
 <div class="col-md-4">
     <div class="text-center">
         <h1>Leandro</h1>
-        <h3>Fundador, CEO, CTO, e Programador Full Stack</h3>
+        <h3>Fundador, CEO, CTO, e Principal Programador Full Stack</h3>
     </div>
 </div>
 <div class="col-md-4">
